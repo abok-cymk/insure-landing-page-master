679f456e234dafa9bb7a3c693feca55af61dfb77
abok-cymk <allanabok18@gmail.com>
2025-05-27 15:46:37 +0300

    added onClick prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import { memo } from "react";
 
-function Button ({ text, className }) {
+function Button ({ text, className, onClick }) {
     return (
-        <button type="button" className={`border-2 px-4 uppercase cursor-pointer flex items-center justify-center shrink-0 ${className}`}>
+        <button onClick={onClick} type="button" className={`border-2 px-4 uppercase cursor-pointer flex items-center justify-center shrink-0 ${className}`}>
             <span className="sr-only">{text}</span>
             {text}
         </button>
